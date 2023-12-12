8580d6fc5d6f256de82a2340dbf1d933673da1ff
justin-ngatho <93423017+justin-ngatho@users.noreply.github.com>
2023-12-12 06:09:09 +0300

    Updates

---
+++ precache-manifest.e98e644dcfc82c8de921fb03ed39f3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "24536032583b9e9d9c03",
+    "url": "/justin-ngatho/menu-web-app.git/static/css/main.e687af93.chunk.css"
+  },
+  {
+    "revision": "24536032583b9e9d9c03",
+    "url": "/justin-ngatho/menu-web-app.git/static/js/main.24536032.chunk.js"
+  },
+  {
+    "revision": "7fc5741dfb5d402e95fa",
+    "url": "/justin-ngatho/menu-web-app.git/static/js/1.7fc5741d.chunk.js"
+  },
+  {
+    "revision": "d13cd1375d5450cf8400",
+    "url": "/justin-ngatho/menu-web-app.git/static/js/2.d13cd137.chunk.js"
+  },
+  {
+    "revision": "cab262ac923a87405efa",
+    "url": "/justin-ngatho/menu-web-app.git/static/js/runtime~main.cab262ac.js"
+  },
+  {
+    "revision": "6f1e14bb7d419fdddb91a8b47fe1ed0a",
+    "url": "/justin-ngatho/menu-web-app.git/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/justin-ngatho/menu-web-app.git/precache-manifest.e98e644dcfc82c8de921fb03ed39f3b1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/justin-ngatho/menu-web-app.git/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
